6eec7fe444099f6e7195abbb8af4d8b8a3a93758
Vojtěch divoký <divokyvojta@gmail.com>
2025-08-23 13:43:55 +0200

    signature #2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
 <h1>ki-app public</h1>
-<a href="/home">enter app</a><br>
+<a href="/home">enter app</a><br />
+
+<p class="signature">
+	Coded and maintained by <a href="https://divokyvojtech.cz/" target="_blank">Divoký Vojtěch</a>
+</p>
+
+<style>
+    .signature{
+        position: absolute;
+        bottom: 0;
+    }
+</style>
